fd0f19871eed02b2fa90697a08bf86bd22fbbfd0
Daniel Gorbatov <dg20@uw.edu>
2022-06-13 17:51:36 -0700

    add abstract data class

---
+++ lib/src/logic/data/ordinalData.dart
@@ -0,0 +1,10 @@
+import 'package:sushi_scouts/src/logic/data/scoutingData.dart';
+
+class OrdinalData extends ScoutingData {
+  @override
+  String stringfy() {
+    // TODO: implement stringfy
+    throw UnimplementedError();
+  }
+
+}
