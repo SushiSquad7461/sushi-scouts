21139b767b5ecd7be487aea0c91bfe7e94879184
Daniel Gorbatov <dg20@uw.edu>
2022-06-15 18:36:57 -0700

    navagation done with navigator 2.0

---
+++ lib/src/views/ui/Login.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../util/Header/HeaderTitle.dart';
+import '../util/footer.dart';
+import '../util/header/HeaderNav.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(
+          children: const [
+            HeaderTitle(),
+          ],
+        )
+    );
+  }
+}
