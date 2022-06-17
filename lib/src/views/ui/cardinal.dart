eff1ed200c37a9247c4dbc3a52449ebacd1fc044
Sid Raj <rsidharth53@gmail.com>
2022-06-16 20:10:36 -0700

    bug fixes

--- lib/src/views/ui/cardinal.dart
+++ lib/src/views/ui/cardinal.dart
@@ -1,41 +1,67 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:sushi_scouts/src/logic/enums/Pages.dart';
+
+import '../util/Header/HeaderTitle.dart';
+import '../util/footer.dart';
+import '../util/header/HeaderNav.dart';
 import 'package:sushi_scouts/src/logic/data/cardinalData.dart';
 import 'package:sushi_scouts/src/views/util/inputs/NumberInput.dart';
 
-CardinalData? data;
-
 class Cardinal extends StatefulWidget {
-  const Cardinal(Key? key) : super(key: key);
+  CardinalData? data;
+  final ValueChanged changePage;
+  Cardinal({Key? key, required this.changePage}) : super(key: key);
   @override
   Pregame createState() => Pregame();
 }
 
 class Pregame extends State<Cardinal> {
-  void onSubmit(String name, Data value) {
-    data?.setPregame(name, value);
+  List<String>? names;
+
+  Future<bool> setData() async {
+    widget.data = await CardinalData.firstCreate();
+    names = widget.data!.getNames(MatchStage.pregame);
+    return true;
   }
 
-  @override
-  void initState() {
-    if (data == null) {
-      Future.delayed(Duration.zero, () async {
-        data = await CardinalData.firstCreate();
-      });
-    }
-    super.initState();
+  void onSubmit(String name, Data value) {
+    widget.data!.setPregame(name, value);
+    print(widget.data!.getPregame(name));
   }
 
   @override
   Widget build(BuildContext context) {
-    List<String> names = data!.getNames(MatchStage.pregame);
-    return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            Column(
-              children: [NumberInput(name: names[0], onSubmit: onSubmit)],
-            )
-          ],
-        ));
+    return Scaffold(
+        body: ListView(
+      children: [
+        const HeaderTitle(),
+        HeaderNav(
+          currentPage: Pages.ordinal,
+          changePage: widget.changePage,
+        ),
+        FutureBuilder(
+            future: setData(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              return snapshot.hasData
+                  ? Row(
+                      children: [
+                        Column(
+                          children: [
+                            NumberInput(name: names![0], onSubmit: onSubmit)
+                          ],
+                        )
+                      ],
+                    )
+                  : const CircularProgressIndicator();
+            }),
+        const Footer(
+          nextPage: true,
+          previousPage: false,
+        ),
+      ],
+    ));
   }
 }
--- lib/src/views/util/header/HeaderNav.dart
+++ lib/src/views/util/header/HeaderNav.dart
@@ -4,18 +4,20 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sushi_scouts/src/logic/color/HexColor.dart';
+import 'package:sushi_scouts/src/logic/enums/Pages.dart';
+import 'package:enum_to_string/enum_to_string.dart';
 
 class HeaderNav extends StatelessWidget {
-  final List<String> _pages = const ["ORDINAL", "CARDINAL", "PIT", "SETTINGS"];
   final TextStyle _pageStyle = const TextStyle(
     fontFamily: "Sushi",
     fontSize: 25,
     fontWeight: FontWeight.bold,
   );
 
-  final String currentPage;
+  final Pages currentPage;
+  final ValueChanged changePage;
 
-  const HeaderNav({Key? key, required this.currentPage}) : super(key: key);
+  const HeaderNav({Key? key, required this.currentPage, required this.changePage}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -30,23 +32,26 @@ class HeaderNav extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              for (final page in _pages) Padding(
+              for (final page in Pages.values) if (page != Pages.login) Padding(
                 padding: const EdgeInsets.only(left: 0, right: 0, top: 8, bottom: 8),
-                child: Container(
-                  decoration: ((currentPage == page) ?  BoxDecoration(
-                      border: Border.all(color: HexColor("#56CBF9"), width: 5),
-                      borderRadius: const BorderRadius.all(Radius.circular(5)),
-                    ) :  BoxDecoration(
-                      border: Border.all(color: Colors.white)
-                    )
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 4, right: 4, top: 2, bottom: 2),
-                    child: Text(
-                      page,
-                      style: GoogleFonts.mohave(
-                        textStyle: _pageStyle
-                      ),
+                child: GestureDetector(
+                  onTap: () => changePage(page),
+                  child: Container(
+                    decoration: ((currentPage == page) ?  BoxDecoration(
+                        border: Border.all(color: HexColor("#56CBF9"), width: 5),
+                        borderRadius: const BorderRadius.all(Radius.circular(5)),
+                      ) :  BoxDecoration(
+                        border: Border.all(color: Colors.white, width: 5)
+                      )
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 4, right: 4, top: 2, bottom: 2),
+                      child: Text(
+                        EnumToString.convertToString(page).toUpperCase(),
+                        style: GoogleFonts.mohave(
+                          textStyle: _pageStyle
+                        ),
+                      )
                     )
                   )
                 )
