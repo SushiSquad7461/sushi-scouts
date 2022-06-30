0b72f8b535063b569b220e026010e80daf41287b
Daniel Gorbatov <dzgorbatov@gmail.com>
2022-06-29 21:00:21 -0700

    add documentation

--- lib/src/logic/data/Data.dart
+++ lib/src/logic/data/Data.dart
@@ -1,14 +1,21 @@
+/*
+  The Data classes purpose is to store the current value of a component.
+  It currently supports strings and integers.
+*/
 class Data<ValueType> {
+  // Current value of the component
   ValueType currValue;
   bool setByUser;
 
   Data(this.currValue, {this.setByUser = false});
 
+  // Set the current value
   void set(ValueType newVal, {bool setByUser = false}) {
     this.setByUser = setByUser;
     currValue = newVal;
   }
 
+  // Increments the current value if it is an integer and retuns true, otherwise returns false
   bool increment() {
     if (currValue is int) {
       currValue = ((currValue as int) + 1) as ValueType;
@@ -17,6 +24,7 @@ class Data<ValueType> {
     return false;
   }
 
+  // Decrements the current value if it is an integer and retuns true, otherwise returns false
   bool decrement() {
     if (currValue is int) {
       currValue = ((currValue as int) + 1) as ValueType;
@@ -25,12 +33,8 @@ class Data<ValueType> {
     return false;
   }
 
+  // Gets the current value as a string
   String get() {
     return (currValue is int) ? (currValue as int).toString() : (currValue as String);
-  }
-
-  @override
-  String toString() {
-    return get();
   }
 }
