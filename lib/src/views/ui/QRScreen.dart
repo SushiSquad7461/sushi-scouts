d080cc5ff8e0c7e5748640649e9c3ad1dd9c02bb
Daniel Gorbatov <dzgorbatov@gmail.com>
2022-07-08 14:44:23 -0700

    fix continue button style

--- lib/src/views/ui/QRScreen.dart
+++ lib/src/views/ui/QRScreen.dart
@@ -35,16 +35,16 @@ class QRScreen extends StatelessWidget {
               alignment: AlignmentDirectional.center,
               children: [
                 Container(
-                  width: ScreenSize.width*0.9,
-                  height: ScreenSize.width*0.9,
+                  width: ScreenSize.width*0.8,
+                  height: ScreenSize.width*0.8,
                   decoration: BoxDecoration(
                     color: Colors.black,
                     borderRadius: BorderRadius.all(Radius.circular(ScreenSize.width*0.1))
                   ),
                 ),
                 Container(
-                  width: ScreenSize.width*0.75,
-                  height: ScreenSize.width*0.75,
+                  width: ScreenSize.width*0.7,
+                  height: ScreenSize.width*0.7,
                   color: Colors.white,
                 ),
                 SizedBox(
@@ -61,21 +61,18 @@ class QRScreen extends StatelessWidget {
         Align(
                   alignment: Alignment(0, 0.83),
                   child: Container(
-                      width: 200*ScreenSize.swu,
-                      height: 60*ScreenSize.swu,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 4),
-                        color: const Color(0xfafafa),
-                        borderRadius: BorderRadius.circular(20*ScreenSize.swu),
+                      width: ScreenSize.width,
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
                       ),
                       child: TextButton(
                         onPressed: () => changePage(previousPage),                        
                         child: Text(
-                          'Next Match',
+                          'CONTINUE',
                           style: TextStyle(
-                              fontSize: 25*ScreenSize.swu,
+                              fontSize: 35*ScreenSize.swu,
                               fontFamily: "Sushi",
-                              color: Colors.black,
+                              color: Colors.white,
                               fontWeight: FontWeight.bold),
                         ),
                       )),
