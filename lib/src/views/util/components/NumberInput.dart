c20e7b5911dc783a55ba5a0339d62c5a49b7eae1
Sidharth R <rsidharth53@gmail.com>
2022-07-09 10:01:17 -0700

    fixed number input

--- lib/src/views/util/components/NumberInput.dart
+++ lib/src/views/util/components/NumberInput.dart
@@ -3,14 +3,12 @@ import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sushi_scouts/src/logic/data/Data.dart';
 
-class NumberInput extends StatelessWidget {
+class NumberInput extends StatefulWidget {
   final String name;
   final Data data;
   final Color color;
   final Color textColor;
   final double width;
-  late final TextEditingController _controller;
-  late FocusNode _focusNode;
 
   NumberInput(
       {Key? key,
@@ -19,15 +17,7 @@ class NumberInput extends StatelessWidget {
       required this.color,
       required this.width,
       required this.textColor})
-      : super(key: key){
-        _controller = TextEditingController(text: data.setByUser ? data.get().toString() : null);
-        _focusNode = FocusNode();
-        _controller.addListener(() {
-          if(_controller.text != "") {
-            data.set(double.parse(_controller.text), setByUser: true);
-          }
-        });
-      }
+      : super(key: key);
 
   static NumberInput create(Key key, String name, Data data, List<String>? values,
       Data defaultValue, Color color, double width, Color textColor) {
@@ -40,7 +30,23 @@ class NumberInput extends StatelessWidget {
       textColor: textColor
     );
   }
+  @override
+  NumberInputState createState() => NumberInputState();
+}
+class NumberInputState extends State<NumberInput> {
+  late final TextEditingController _controller;
+  late FocusNode _focusNode;
 
+  @override
+  void initState() {
+    _controller = TextEditingController(text: widget.data.setByUser ? widget.data.get().toString() : null);
+    _focusNode = FocusNode();
+    _controller.addListener(() {
+      if(_controller.text != "") {
+        widget.data.set(double.parse(_controller.text), setByUser: true);
+      }});
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -49,12 +55,12 @@ class NumberInput extends StatelessWidget {
       },
       child: Padding(
           padding: EdgeInsets.only(
-              left: width / 60,
-              right: width / 60,
-              top: width / 30,
-              bottom: width / 30),
+              left: widget.width / 60,
+              right: widget.width / 60,
+              top: widget.width / 30,
+              bottom: widget.width / 30),
           child: SizedBox(
-              width: width * 0.7,
+              width: widget.width * 0.7,
               child: Column(children: [
                 Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                   // Text(name.toUpperCase(),
@@ -72,25 +78,25 @@ class NumberInput extends StatelessWidget {
                           controller: _controller,
                           decoration: InputDecoration(
                             border: InputBorder.none,
-                            constraints: BoxConstraints(maxWidth: width / 6.0),
-                            hintText: name.toUpperCase(),
+                            constraints: BoxConstraints(maxWidth: widget.width / 6.0),
+                            hintText: widget.name.toUpperCase(),
                             hintStyle: const TextStyle(color: Colors.black),
                           ),
                           style: GoogleFonts.mohave(
                               textStyle: TextStyle(
-                                  fontSize: width / 8,
+                                  fontSize: widget.width / 8,
                                   fontWeight: FontWeight.w400,
-                                  color: textColor)),
+                                  color: widget.textColor)),
                           textAlign: TextAlign.left,
                           keyboardType: TextInputType.number,
                           inputFormatters: <TextInputFormatter>[
                             FilteringTextInputFormatter.digitsOnly
                           ],
                           onFieldSubmitted: (value) {
-                            data.set(double.parse(value), setByUser: true);
+                            widget.data.set(double.parse(value), setByUser: true);
                           }))
                 ]),
-                Divider(color: textColor, thickness: width / 50)
+                Divider(color: widget.textColor, thickness: widget.width / 50)
               ]))),
     );
   }
